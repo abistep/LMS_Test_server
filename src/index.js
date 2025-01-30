65f890338ca8e7b3d49002c5a58d2a7657df261f
misterT1A <paradogss@mail.ru>
2025-01-30 15:10:56 +0300

    feat: added tg bot

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 // const jwt = require("jsonwebtoken");
 
+const { TELEGRAM_BOT_TOKEN } = require("./constants");
+
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
@@ -25,3 +29,45 @@ app.use(cors(corsOptions));
 app.use("/api", require("./routes/routes"));
 
 app.listen(port, () => console.log("Сервер запущен на http://localhost:3000"));
+
+const start = () => {
+  bot.setMyCommands([
+    { command: "/info", description: "Information" },
+    { command: "/game", description: "Game" },
+    { command: "/shop", description: "Shop" },
+  ]);
+
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+
+    if (text === "/info") {
+      await bot.sendMessage(chatId, `Your name ${msg.from.first_name}`);
+    }
+
+    if (text === "/shop") {
+      bot.sendMessage(
+        chatId,
+        "Нажмите на кнопку ниже, чтобы открыть магазин:",
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "Открыть магазин",
+                  web_app: {
+                    url: `https://willowy-pastelito-e13e4c.netlify.app`,
+                  },
+                },
+              ],
+            ],
+          },
+        }
+      );
+      return;
+    }
+  });
+};
+
+start();
